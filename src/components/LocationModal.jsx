d8bb910c678f2c206322b5a9d0af269677cc9595
sujunprodhan <144489247+sujunprodhan@users.noreply.github.com>
2026-09-14 01:49:55 +0600

    locatin setup

--- src/components/LocationModal.jsx
+++ src/components/LocationModal.jsx
@@ -1,8 +1,70 @@
-const LocationModal = () => {
+import { X } from 'lucide-react';
+import { useState } from 'react';
+
+const LocationModal = ({ onClose }) => {
+  const [city, setCity] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = city.trim();
+    console.log(value);
+    setCity('');
+  };
+
+  const handleLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        console.log(latitude, longitude);
+      },
+      (error) => {
+        console.log(error);
+      },
+      {
+        timeout: 10000,
+      }
+    );
+  };
+
   return (
-    <div className="h-75 fixed inset-0 items-center mx-auto w-md bg-gray-100 rounded-md shadow-lg shadow-gray-300 mt-10">
-      <div>
-        <h1 className="text-black text-2xl font-semibold mt-3">Where are you today</h1>
+    <div className="fixed inset-0 items-center bg-gray-950/60">
+      <div className="h-75  mx-auto w-md bg-gray-100 rounded-md shadow-md shadow-gray-400 mt-10">
+        <div className="flex justify-between text-center p-5">
+          <h1 className="text-black text-2xl font-semibold">Where are you today</h1>
+          <button
+            onClick={onClose}
+            className="bg-red-600 p-1 cursor-pointer hover:bg-red-400 text-white rounded-md"
+          >
+            <X />
+          </button>
+        </div>
+        <form className="space-y-3" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="input city"
+            className="border border-gray-400 rounded-md p-1 outline-0"
+          />
+
+          <div>
+            <button
+              type="submit"
+              className="bg-orange-500 text-white font-semibold p-2 rounded-md hover:scale-105 transition-all cursor-pointer"
+            >
+              Get Weather
+            </button>
+          </div>
+          <h3>Or</h3>
+          <div>
+            <button
+              type="button"
+              onClick={handleLocation}
+              className="bg-orange-500 text-white font-semibold p-2 rounded-md hover:scale-105 transition-all cursor-pointer"
+            >
+              Use Location
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,20 @@
 import { useState } from 'react';
 import LocationModal from '../components/LocationModal';
 
-
 const Home = () => {
-  const [click, setClick] = useState('');
-  console.log(click);
-  
- 
-  
+  const [click, setClick] = useState(false);
+
   return (
     <div className="w-11/12 mx-auto text-capitalize text-center text-gray-400">
       <p>check your weather today </p>
       <button
-        onClick={() => setClick('clicked')}
+        onClick={() => setClick(true)}
         type="button"
         className="bg-orange-500 text-white font-semibold p-2 rounded-md mt-5 hover:scale-105 transition-all cursor-pointer "
       >
         Check Weather
       </button>
-      {click && <LocationModal/>}
+      {click && <LocationModal onClose={() => setClick(false)} />}
     </div>
   );
 };
